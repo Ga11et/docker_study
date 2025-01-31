9d929d04debfe3a7fccf82a158da60aa1ff79d59
fyodor <sivkov1998@yandex.ru>
2025-01-31 17:40:24 +0300

    created Http class

--- project/api/src/Http/Action/HomeAction.php
+++ project/api/src/Http/Action/HomeAction.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace App\Http\Action;
 
+use App\Http;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use stdClass;
+
+
 
 class HomeAction
 {
   public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args) {
-    $response->getBody()->write('{ "message": "Hello world" }');
-    return $response->withHeader('Content-Type', 'application/json');
+    return Http::json($response, new stdClass());
   }
 }
