e04b553e299ad75fb4fa960af474224ea2ba7e6c
fyodor <sivkov1998@yandex.ru>
2025-02-13 17:05:00 +0300

    added di container and JsonResponse class

--- project/api/public/index.php
+++ project/api/public/index.php
@@ -3,43 +3,17 @@
 declare(strict_types=1);
 
 use App\Http;
-use Psr\Container\ContainerInterface;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$definitions = [
-  'config' => [
-      'debug' => (bool)getenv('APP_DEBUG'),
-  ]
-];
-
-$container = new class($definitions) implements ContainerInterface {
-  private $definitions;
-
-  public function __construct(array $definitions)
-  {
-      $this->definitions = $definitions;
-  }
-
-  public function get($id)
-  {
-      if (isset($this->definitions[$id])) {
-          $definition = $this->definitions[$id];
-          if (is_callable($definition)) {
-              return $definition($this);
-          }
-          return $definition;
-      }
-
-      throw new \InvalidArgumentException("No entry or class found for '$id'");
-  }
-
-  public function has($id): bool
-  {
-      return isset($this->definitions[$id]);
-  }
-};
+$builder = new DI\ContainerBuilder();
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool) getenv('APP_DEBUG'),
+    ]
+]);
+$container = $builder->build();
 
 $app = AppFactory::createFromContainer($container);
 
--- project/api/src/Http/Action/HomeAction.php
+++ project/api/src/Http/Action/HomeAction.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace App\Http\Action;
 
-use App\Http;
+use App\Http\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use stdClass;
 
-
-
-class HomeAction
+class HomeAction implements RequestHandlerInterface
 {
-  public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args) {
-    return Http::json($response, new stdClass());
+  public function handle(ServerRequestInterface $request): ResponseInterface
+  {
+    return new JsonResponse(new stdClass());
   }
 }
